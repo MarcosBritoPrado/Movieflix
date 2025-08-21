29dd393522c145494e5fae53e8cfde53dafac69b
MarcosBritoPrado <marcos.vinicius05@souunit.com.br>
2025-08-21 13:59:43 -0300

    feat: Criado Category - controller, service, repository, mapper, request,response

---
+++ src/main/java/br/com/movieflix/controller/response/CategoryResponse.java
@@ -0,0 +1,8 @@
+package br.com.movieflix.controller.response;
+
+import lombok.Builder;
+
+@Builder
+public record CategoryResponse(String name, Long id) {
+
+}
---
+++ src/main/java/br/com/movieflix/repository/CategoryRepository.java
@@ -0,0 +1,12 @@
+package br.com.movieflix.repository;
+
+import br.com.movieflix.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    List<Category> id(Long id);
+}
