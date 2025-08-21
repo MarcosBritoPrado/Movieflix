28de664b47f6f99884714fdd278e2a64b234ebea
MarcosBritoPrado <marcos.vinicius05@souunit.com.br>
2025-08-21 17:24:07 -0300

    feat: Criado Movie - controller, service, repository, mapper, request,response

--- src/main/java/br/com/movieflix/repository/MovieRepository.java
+++ src/main/java/br/com/movieflix/repository/MovieRepository.java
@@ -1,9 +1,9 @@
 package br.com.movieflix.repository;
 
-import br.com.movieflix.entity.Streaming;
+import br.com.movieflix.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StreamingRepository extends JpaRepository<Streaming, Long> {
+public interface MovieRepository extends JpaRepository<Movie, Long> {
 }
