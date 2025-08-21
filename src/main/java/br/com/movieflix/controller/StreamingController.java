efe7f465afb9e97f703e36d098052c76af67e5b4
MarcosBritoPrado <marcos.vinicius05@souunit.com.br>
2025-08-21 15:35:39 -0300

    feat: Criado Streaming - controller, request, response, repositoru, service

---
+++ src/main/java/br/com/movieflix/controller/StreamingController.java
@@ -0,0 +1,52 @@
+package br.com.movieflix.controller;
+
+import br.com.movieflix.controller.request.StreamingRequest;
+import br.com.movieflix.controller.response.StreamingResponse;
+import br.com.movieflix.entity.Streaming;
+import br.com.movieflix.mapper.StreamingMapper;
+import br.com.movieflix.service.StreamingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/movieflix/streaming")
+public class StreamingController {
+
+    private final StreamingService streamingService;
+
+
+    @GetMapping()
+    public ResponseEntity<List<StreamingResponse>> getAllStreamings(){
+        List<StreamingResponse> streamings = streamingService.findAll()
+                .stream()
+                .map(StreamingMapper::toStreamingResponse)
+                .toList();
+
+        return ResponseEntity.ok(streamings);
+    }
+    @PostMapping
+    public ResponseEntity<StreamingResponse> saveCategory(@RequestBody StreamingRequest request){
+        Streaming newStreaming = StreamingMapper.toStreaming(request);
+        Streaming savedStreaming = streamingService.saveStreaming(newStreaming);
+        return ResponseEntity.status(HttpStatus.CREATED).body(StreamingMapper.toStreamingResponse(savedStreaming));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<StreamingResponse> getByCategoryId(@PathVariable("id") Long id){
+        return streamingService.findById(id)
+                .map(streaming -> ResponseEntity.ok(StreamingMapper.toStreamingResponse(streaming)))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCategoryById(@PathVariable("id") Long id) {
+        streamingService.deleteById(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+}
---
+++ src/main/java/br/com/movieflix/entity/Streaming.java
@@ -0,0 +1,20 @@
+package br.com.movieflix.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "streaming")
+public class Streaming {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
