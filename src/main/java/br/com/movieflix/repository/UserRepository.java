b007436b835e711df94d8066acc56521a2f6ebe7
MarcosBritoPrado <marcos.vinicius05@souunit.com.br>
2025-08-22 18:04:10 -0300

    feat(auth): add login flow, auth service and config updates

--- src/main/java/br/com/movieflix/repository/UserRepository.java
+++ src/main/java/br/com/movieflix/repository/UserRepository.java
@@ -2,6 +2,12 @@ package br.com.movieflix.repository;
 
 import br.com.movieflix.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<UserDetails> findUserByEmail(String email);
 }
